15dd7953b198b7ec8d510e50bee39e8f7b747d61
Dan Whitford <dan.whitford@fanduel.com>
2022-07-22 16:14:12 +0100

    compiler/scanner start

--- vm.c
+++ vm.c
@@ -2,6 +2,7 @@
 
 #include "common.h"
 #include "vm.h"
+#include "compiler.h"
 #include "debug.h"
 
 VM vm;
@@ -91,9 +92,8 @@ static InterpretResult run()
 #undef BINARY_OP
 }
 
-InterpretResult interpret(Chunk *chunk)
+InterpretResult interpret(const char* source)
 {
-    vm.chunk = chunk;
-    vm.ip = vm.chunk->code;
-    return run();
+    compile(source);
+    return INTERPRET_OK;
 }
